bc305159a942c4126391fc7e1cc504d453afe11d
s-ourav <sourav.mozu@gmail.com>
2023-12-29 18:23:47 +0530

    fifteenth

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,5 +1,5 @@
 package com.driver;
-import java.util.*;
+
 //import static jdk.internal.org.jline.utils.Colors.s;
 
 public class CurrentAccount extends BankAccount{
@@ -28,71 +28,39 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-        if(!isValid(this.tradeLicenseId))
-        {
-            String ans=orgString(this.tradeLicenseId);
-            if(ans.length()==0)
-            {
-                throw new Exception("Valid License can not be generated");
-            }
-            else
-            {
-                this.tradeLicenseId=ans;
+        boolean valid=true;
+        for(int i=0;i<tradeLicenseId.length()-1;i++) {
+            if (tradeLicenseId.charAt(i + 1) == tradeLicenseId.charAt(i)) {
+                valid = false;
+                break;
             }
         }
-    }
-    private boolean isValid(String s) {
-        for(int i=1;i<s.length();i++)
-        {
-            if(s.charAt(i-1)==s.charAt(i))
-            {
-                return false;
+            if(!valid) {
+                StringBuilder sb=new StringBuilder();
+                int freq[]=new int[26];
+                for(char ch : tradeLicenseId.toCharArray()){
+                    freq[ch-'a'] ++;
+                }
+                int n=tradeLicenseId.length();
+                for(int i=0;i<n;i++){
+                    char maxc=' ';
+                    int max=0;
+                    for(int j=0;j<26;j++){
+                        if(sb.length()>0 && j==sb.charAt(i-1)-'a') continue;
+                        if(freq[j]>max){
+                            max=freq[j];
+                            maxc= (char)('a'+j);
+                        }
+                    }
+                    if(maxc==' ')
+                        throw new Exception("Valid License can not be generated");
+                    sb.append(maxc);
+                    // System.out.println(maxc);
+                    freq[(int)maxc-'a']--;
+                }
+                tradeLicenseId=new String(sb.toString());
             }
-        }
-        return true;
-    }
-    class pair{
-        char ch;
-        int freq;
-        pair(char ch,int freq)
-        {
-            this.ch=ch;
-            this.freq=freq;
+
         }
     }
-    private String orgString(String s) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        PriorityQueue<pair>pq=new PriorityQueue<>((a,b)->{
-            return a.freq>b.freq ? -1 : 1;
-        });
-        StringBuilder sb=new StringBuilder();
-        for(char ch:s.toCharArray())
-        {
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        for(char key:map.keySet())
-        {
-            pq.add(new pair(key,map.get(key)));
-        }
-        pair prev=pq.remove();
-        sb.append(prev.ch);
-        prev.freq--;
-        while(pq.size()>0)
-        {
-            pair curr=pq.remove();
-            sb.append(curr.ch);
-            curr.freq--;
-            if(prev.freq>0)
-            {
-                pq.add(prev);
-            }
-            prev=curr;
-        }
-        if(sb.length()!=s.length())
-        {
-            return "";
-        }
-        return sb.toString();
-    }
-}
 
